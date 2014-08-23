1ac917fe8ea1128ade989c2e0b7178c57919ee9d
modestmonk <mpjohns2@mail.usf.edu>
2014-08-23 18:34:20 -0400

    Merge

---
+++ Assets/Scripts/TitleScreenScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TitleScreenScript : MonoBehaviour
+{
+    public List<MyButton> ButtonList;
+    private int currentIndex = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ButtonList[currentIndex].Select();
+        if (Input.GetButtonDown("DownKey"))
+        {
+            currentIndex++;
+        }
+        if (currentIndex >= ButtonList.Count)
+        {
+            currentIndex = 0;
+        }
+    }
+}
