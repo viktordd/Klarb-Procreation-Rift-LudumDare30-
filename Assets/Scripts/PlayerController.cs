bc156dcad2c45d00dea49e83a3fd96140ccf823a
LogicaLInsanity <ljerez@mail.usf.edu>
2014-08-23 00:58:53 -0400

    Init Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float moveSpeed = 8.0f;
+    public string player;
+    private string horizontal = string.Empty;
+    private string vertical = string.Empty;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    horizontal = "Horizontal" + player;
+	    vertical = "Vertical" + player;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    PlayerInput();          
+	}
+
+    private void PlayerInput()
+    {
+        float haxis = Input.GetAxis(horizontal);
+        float vaxis = Input.GetAxis(vertical);
+        transform.Translate(new Vector3(haxis, vaxis) * moveSpeed * Time.deltaTime);
+        
+        //if (Input.GetButton("LeftLeft"))
+        //{         
+        //    transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetButton("LeftUp"))
+        //{         
+        //    transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetButton("LeftDown"))
+        //{         
+        //    transform.Translate(-Vector2.up * moveSpeed * Time.deltaTime);
+        //}
+    }
+}
