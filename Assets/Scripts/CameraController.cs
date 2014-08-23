acdbc1e0a2edb1175bd62374921f39c0930d930e
LogicaLInsanity <ljerez@mail.usf.edu>
2014-08-23 01:51:11 -0400

    Initial Camera Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform PlayerLeft;
+    public Transform PlayerRight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(0, (PlayerLeft.position.y - PlayerRight.position.y) / 2, -10);
+	}
+}
