5d3511e779a4c505426c7c4449477da44bdc695d
viktordd <Viktordd@gmail.com>
2014-08-23 03:15:53 -0400

    Player Tile Colision and Prefabs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Runtime.InteropServices;
 using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
 
     private float moveSpeed = 8.0f;
+
     public string player;
     private string horizontal = string.Empty;
     private string vertical = string.Empty;
+
+	private bool grounded = false;
+	public Transform groundCheck;
+	private float groundRadius = 0.02f;
+	public LayerMask whatIsGround;
 
 	// Use this for initialization
 	void Start ()
@@ -20,26 +27,28 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		Debug.Log(grounded ? "Enter" : "Falling");
 	    PlayerInput();          
 	}
 
-    private void PlayerInput()
-    {
-        float haxis = Input.GetAxis(horizontal);
-        float vaxis = Input.GetAxis(vertical);
-        transform.Translate(new Vector3(haxis, vaxis) * moveSpeed * Time.deltaTime);
-        
-        //if (Input.GetButton("LeftLeft"))
-        //{         
-        //    transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
-        //}
-        //if (Input.GetButton("LeftUp"))
-        //{         
-        //    transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
-        //}
-        //if (Input.GetButton("LeftDown"))
-        //{         
-        //    transform.Translate(-Vector2.up * moveSpeed * Time.deltaTime);
-        //}
-    }
+	private void PlayerInput()
+	{
+		float haxis = Input.GetAxis(horizontal);
+		float vaxis = Input.GetAxis(vertical);
+		transform.Translate(new Vector3(haxis, vaxis) * moveSpeed * Time.deltaTime);
+
+		//if (Input.GetButton("LeftLeft"))
+		//{         
+		//    transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+		//}
+		//if (Input.GetButton("LeftUp"))
+		//{         
+		//    transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+		//}
+		//if (Input.GetButton("LeftDown"))
+		//{         
+		//    transform.Translate(-Vector2.up * moveSpeed * Time.deltaTime);
+		//}
+	}
 }
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//void OnTriggerEnter2D(Collider2D otherObject)
+	//{
+	//	if (otherObject.gameObject.tag == "Player")
+	//	{
+	//		Debug.Log("Enter");
+	//	}
+	//}
+
+	//void OnTriggerExit2D(Collider2D otherObject)
+	//{
+	//	if (otherObject.gameObject.tag == "Player")
+	//	{
+	//		Debug.Log("Exit");
+	//	}
+	//}
+}
