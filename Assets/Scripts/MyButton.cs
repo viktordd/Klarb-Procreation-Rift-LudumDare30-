0ed716f43a035b352f7a4b89a2ce347bb077f8ec
TheDorf <jhwolf1728@gmail.com>
2014-08-23 18:27:03 -0400

    OH WELL

---
+++ Assets/Scripts/MyButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyButton : MonoBehaviour {
+
+    public Sprite[] SpriteCollection;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseEnter()
+    {
+        Select();
+    }
+    void OnMouseExit()
+    {
+        Deselect();
+    }
+    void OnMouseDown()
+    {
+        Hit();
+    }
+
+    public void Select()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = SpriteCollection[1];
+    }
+    public void Deselect()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = SpriteCollection[0];
+    }
+    public void Hit()
+    {
+        Application.LoadLevel("SlideScene");
+    }
+}
