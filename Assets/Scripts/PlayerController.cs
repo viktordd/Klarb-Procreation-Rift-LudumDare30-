cf4121a769319dce11659a91e39d1257af2d4415
modestmonk <mpjohns2@mail.usf.edu>
2014-08-24 17:03:39 -0400

    v

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	private float moveSpeed = 5f;
+	private float moveSpeed = 3f;
 
     public string player;
     private string horizontal = string.Empty;
