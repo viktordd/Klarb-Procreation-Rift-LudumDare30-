666a845c5e36756087e6bb9ce8b3699794b65d99
TheDorf <jhwolf1728@gmail.com>
2014-08-24 14:20:57 -0400

    Merge branch 'master' of https://github.com/L0G1C/LudumDare30
    Conflicts:
    	Assets/Scripts/ChunkManager.cs

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,31 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TileController : MonoBehaviour {
+public class TileController : MonoBehaviour
+{
+	private Animator anim;
+	private bool falling = false;
+	private PlayerController player;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!falling && player != null && !player.Jumping)
+		{
+			StartFalling();
+		}
 	}
 
-	//void OnTriggerEnter2D(Collider2D otherObject)
-	//{
-	//	if (otherObject.gameObject.tag == "Player")
-	//	{
-	//		Debug.Log("Enter");
-	//	}
-	//}
+	void OnTriggerEnter2D(Collider2D otherObject)
+	{
+		if (otherObject.CompareTag("Player"))
+		{
+			PlayerController pl = otherObject.GetComponent<PlayerController>();
+			if (pl.Jumping)
+			{
+				player = pl;
+			}
+			else
+				StartFalling();
+		}
+	}
 
-	//void OnTriggerExit2D(Collider2D otherObject)
-	//{
-	//	if (otherObject.gameObject.tag == "Player")
-	//	{
-	//		Debug.Log("Exit");
-	//	}
-	//}
+	void OnTriggerExit2D(Collider2D otherObject)
+	{
+		if (!falling && otherObject.CompareTag("Player"))
+		{
+			player = null;
+		}
+	}
+
+	private void StartFalling()
+	{
+		falling = true;
+		anim.SetBool("Falling", true);
+	}
+
+	public void EndFalling()
+	{
+		Destroy(gameObject);
+	}
 }
