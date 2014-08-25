0c6f7c10fb7e3ab4e492ceaa8f44796fa191aa18
LogicaLInsanity <ljerez@mail.usf.edu>
2014-08-24 21:56:34 -0400

    MusicManager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+
+    private static MusicManager instance = null;
+    public static MusicManager Instance { get { return instance; } }
+    public static AudioClip SongName;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MusicManager_NewSong.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager_NewSong : MonoBehaviour
+{
+
+    public AudioClip NewMusic;
+    public AudioClip RandomBonusMusic;
+
+	// Use this for initialization
+	void Awake ()
+	{
+	    if (RandomBonusMusic)
+	    {
+	        var rand = Random.Range(0, 2);
+	        if (rand == 1)
+	            NewMusic = RandomBonusMusic;
+	    }
+
+
+	    var gameMusic = GameObject.Find("GameMusic");
+	    gameMusic.audio.clip = NewMusic;
+        gameMusic.audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
