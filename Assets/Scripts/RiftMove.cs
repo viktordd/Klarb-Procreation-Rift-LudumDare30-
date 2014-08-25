9ee37f6ec0032dce8da470206ef0b65a56758897
LogicaLInsanity <ljerez@mail.usf.edu>
2014-08-25 16:51:07 -0400

    Added rift. Created Start/End Tiles

---
+++ Assets/Scripts/RiftMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RiftMove : MonoBehaviour
+{
+
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    this.transform.position = new Vector3(0, camera.transform.position.y);
+	}
+}
