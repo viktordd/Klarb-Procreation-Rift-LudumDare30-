35c4d16ebedf7c72c2a2eef2859ee95fcd185e87
LogicaLInsanity <ljerez@mail.usf.edu>
2014-08-25 19:22:36 -0400

    didn't save LevelNumber b efore i guess

--- Assets/Scripts/LevelNumber.cs
+++ Assets/Scripts/LevelNumber.cs
@@ -4,11 +4,22 @@ using System.Collections;
 public class LevelNumber : MonoBehaviour
 {
 
-
-    public int LevelDiffNumber;
+    private static LevelNumber instance = null;
+    public static LevelNumber Instance { get { return instance; } }
+    public int LevelDiffNumber { get; set; }
 
 	// Use this for initialization
 	void Awake () {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
         DontDestroyOnLoad(this.gameObject);
 	}
 	
